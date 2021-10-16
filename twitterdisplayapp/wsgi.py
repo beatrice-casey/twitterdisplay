ead178236958f26ab032efc0663eebb7b435e7a6
Beatrice Casey <beatrice.casey@gmail.com>
2021-10-16 12:58:08 -0400

    close

--- twitterdisplayapp/wsgi.py
+++ twitterdisplayapp/wsgi.py
@@ -7,7 +7,7 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "twitterdisplayapp.settings")
 
 from django.core.wsgi import get_wsgi_application
 
