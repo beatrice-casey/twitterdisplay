4235d744611aa9baf2544413de9aad581d540ee9
NickCaravias <nick.caravias@gmail.com>
2021-10-27 15:15:37 -0400

    update views and models

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,11 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+
+class Tweet(models.Model):
+    num = models.IntegerField()
+    html = models.TextField()
+    
+    def __str__(self):
+        return "Tweet"
+
--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
-from .models import Greeting
+from .models import Greeting, Tweet
 
 # Create your views here.
+
 def index(request):
     # return HttpResponse('Hello from Python!')
-    return render(request, "index.html")
+    context = {}
+
+    context["dataset"] = Tweet.objects.all()
+
+    return render(request, "index.html", context)
+
+
+def api_response(request):
+    pass
 
 
 def db(request):
