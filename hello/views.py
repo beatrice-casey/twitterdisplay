3709a8ea6addffe77df57e711202dc1217be075f
Justin Schaumberger <jes083@bucknell.edu>
2021-11-03 15:46:18 -0400

    Please work

--- hello/views.py
+++ hello/views.py
@@ -1,54 +1,53 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from urllib.parse import urlencode
 
-#.models import Greeting, Tweet
-import Greeting, Tweet
+import requests
+import tweepy
+from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
+from tweepy import API, OAuthHandler
+
+# CONSTANTS
+BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAAEPxTgEAAAAAqnem0D0Cf8c1zRJ1AgKrN6wiHRI%3DfHNJuInuMppQGzaicMdr4ds7hfOlHNySVwrdrNRvECFMm6bFE1'
+CONSUMER_KEY = "dFGI5f6OBN0QfprVQZmBhA40r"
+CONSUMER_SECRET = "K5dvFQ2h9T1mXvOzrhS1why4COACEUuUssSJZryajWZS0mB1Ho"
+ACCESS_TOKEN = "1436417414728663041-ypu8oIwt1oArCgwsPolWqSjnSnJX5R"
+ACCESS_TOKEN_SECRET = "IMwmI0nIWGMeCbdeEEJwEn3a6WnPVDaC2sfXfiHGa5ZFX"
+
+# the hashtag that needs to be included for certain accounts
+HASHTAG = "BucknellCSNews"
+# the number of tweets to pull from each user
+TWEETS_PER_USER = 10
+
+# GLOBAL VARIABLES
+auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+api = tweepy.API(auth)
+
+
 
 def index(request):
     # return HttpResponse('Hello from Python!')
-    context = {}
-
-    context["dataset"] = Tweet.objects.all()
-
-    return render(request, "index.html", context)
-
-
-def api_response(request):
-    pass
-
-
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, "db.html", {"greetings": greetings})
+    return render(request, "index.html")
 
 
 def build_account_list():
-    # account_list = []
+    # comment
+    account_list = []
     # # extract users from txt file
-    # file = open('users.txt', 'r')
-    # count = 0
-    # while True:
-    #     count += 1
+    with open("accounts.csv",'r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            if line_count == 0:
+                continue
+            else:
+                account_list.append((row[0],row[1]))
+                #print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+                line_count += 1
+    
 
-    #     # Get next line from file
-    #     line = file.readline().strip()
 
-    #     # if line is empty
-    #     # end of file is reached
-    #     if not line:
-    #         break
-    #     account_list.append(line)
-    #     # print("Line{}: {}".format(count, line.strip()))
-    # file.close()
-
-    account_list = ["BucknellCS"]
 
     return account_list
 
@@ -68,38 +67,45 @@ def run():
     # build the list of users to extract
     account_list = build_account_list()
 
-    # go through each user
-    for account in account_list:
-        try:
+    users = []
+    htmls =[]
+    dates = []
 
-            print("Getting data for " + account)
+
+    # go through each user
+    for account_entry in account_list:
+        try:
+            account = account_entry[0]
+            isHashtagOnly = account[1] == 1
+            #print("Getting data for " + account)
             # get the tweetss
-            statuses = api.user_timeline(screen_name=account, count=10, trim_user=True, exclude_replies=True)
+            statuses = api.user_timeline(screen_name=account, count=TWEETS_PER_USER, trim_user=True, exclude_replies=True)
             # loop through the tweets
             for status in statuses:
                 # obtain the tweet url from the json request
                 url = "https://twitter.com/" + account + "/status/" + status._json['id_str']
                 html = generate_html(url)
-                # print(status._json.keys())
                 if html != "":
-                    # time_created_at = time the Tweet was made
-                    # account = username of the account
-                    # html = the html to embed the Tweet
-                    date = generateDate(status)
-
-                    # print(status._json['text'])
-
                     # Working with hashtags
                     hashtags = []
                     for entry in status._json['entities']['hashtags']:
                         hashtags.append(entry['text'])
-                    # print(hashtags)
 
-                    print(html)
+                    # Put in DB
+                    if(not isHashtagOnly or HASHTAG in hashtags):
+                        # time_created_at = time the Tweet was made
+                        # account = username of the account
+                        # html = the html to embed the Tweet
+                        date = generateDate(status)
+                        users.append(account)
+                        htmls.append(html)
+                        dates.append(date)
                 else:
                     print("Not able to get Tweet for " + account)
         except:
             continue
+
+    return users,htmls,dates
 
 
 def generateDate(status):
@@ -132,5 +138,10 @@ def generateDate(status):
     elif month == "Dec":
         month = 12
     date = day + "/" + str(month) + "/" + year
-    return date    
+    return date
+
+
+            
+
+# for testing purposes (to run just the single file)
 run()
