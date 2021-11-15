bea65ef62218302bcf922ec411bd2a45e4b46ba3
NickCaravias <nick.caravias@gmail.com>
2021-11-15 16:04:21 -0500

    pls

--- hello/views.py
+++ hello/views.py
@@ -68,9 +68,11 @@ def start_pgsql():
 def add_to_db(cursor, conn):
     num = 1
     html = '<blockquote class="twitter-tweet"><p lang="en" dir="ltr">Sunsets don&#39;t get much better than this one over <a href="https://twitter.com/GrandTetonNPS?ref_src=twsrc%5Etfw">@GrandTetonNPS</a>. <a href="https://twitter.com/hashtag/nature?src=hash&amp;ref_src=twsrc%5Etfw">#nature</a> <a href="https://twitter.com/hashtag/sunset?src=hash&amp;ref_src=twsrc%5Etfw">#sunset</a> <a href="http://t.co/YuKy2rcjyU">pic.twitter.com/YuKy2rcjyU</a></p>&mdash; US Department of the Interior (@Interior) <a href="https://twitter.com/Interior/status/463440424141459456?ref_src=twsrc%5Etfw">May 5, 2014</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'
-    cursor.execute(f"INSERT INTO hello_tweet ({num}, '{html}')")
-    cursor.fetchall()
+    #cursor.execute(f"INSERT INTO hello_tweet ({num}, '{html}')")
+    cursor.execute("INSERT INTO hello_tweet (num, html) VALUES(%d, %s)", (num, html))
+    
     conn.commit()
+    cursor.fetchall()
 
 def get_from_db(cursor):
     cursor.execute(f'SELECT * from hello_tweet')
