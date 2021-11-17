8aa52a15bc4af649d7f24f3466106c71473e4778
Beatrice Casey <beatrice.casey@gmail.com>
2021-11-17 16:01:28 -0500

    this time

--- hello/views.py
+++ hello/views.py
@@ -78,7 +78,7 @@ def add_to_db(cursor, conn):
         delete_from_db(cursor, len(users)) 
   
     for i in range(len(html)):
-        cursor.execute("INSERT INTO hello_tweet (user, date, html) VALUES(%s, %s)", (users[i], dates[i], html[i]))
+        cursor.execute("INSERT INTO hello_tweet (user, date, html) VALUES(%s, %s, %s)", (users[i], dates[i], html[i]))
     
     conn.commit()
 
