b2f6aca5d96436e8942456efb24fe5e427e2cb64
NickCaravias <nick.caravias@gmail.com>
2021-11-19 15:14:44 -0500

    changed models class name

--- hello/views.py
+++ hello/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 # Create your views here.
 from tweepy import API, OAuthHandler
-from hello.models import Tweet
+from hello.models import Tweet, Username
 import schedule
 import time
 import csv
